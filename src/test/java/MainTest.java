cb14247803ed4ed4c123b0c195f02f4b998b0769
Eugene Kravchenko <zzzheka97@gmail.com>
2020-04-03 19:09:33 +0300

    add get-inclusions option

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,7 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
@@ -12,11 +12,11 @@ public class MainTest {
     public void serializationTest() {
         String inputString1 =
                 ":add build/resources/test/testCase1/include.cpp\n"
-                + ":save dump";
+                        + ":save dump";
         String inputString2 =
                 ":lines\n"
-                + ":load dump\n"
-                + ":lines";
+                        + ":load dump\n"
+                        + ":lines";
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
